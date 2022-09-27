77fbfe372f89a9b5395b2b5dfb21fae13b74709c
PradeepKumar <PradeepKumarRebbavarapu705@gmail.com>
2022-09-28 00:44:24 +0530

    i'

--- components/CreditCard.js
+++ components/CreditCard.js
@@ -5,37 +5,7 @@ export default function CreditCard() {
     const [CreditCardState,setCreditCardState] = useState({number:'',name:'',expiry:'',cvc:'',focus:''})
   return (
     <div id="creditcard">
-        <Cards number={CreditCardState.number} name={CreditCardState.name} expiry={CreditCardState.expiry} cvc={CreditCardState.cvc} focused={CreditCardState.focus}  />
-      <form>
-            <input type="tel" name="number" placeholder='Card Number' value={CreditCardState.number} onChange={(e)=>{
-                setCreditCardState({...CreditCardState,number:e.target.value})
-            }} 
-             onFocus={(e)=>{
-                setCreditCardState({...CreditCardState,focus:e.target.name})
-             }}
-            />
-            <input type="text" name="name" placeholder='Name' value={CreditCardState.name} onChange={(e)=>{
-                setCreditCardState({...CreditCardState,name:e.target.value})
-            }} 
-             onFocus={(e)=>{
-                setCreditCardState({...CreditCardState,focus:e.target.name})
-             }}
-            />
-            <input type="text" name="expiry" placeholder='MM/YY Expiry' value={CreditCardState.expiry} onChange={(e)=>{
-                setCreditCardState({...CreditCardState,expiry:e.target.value})
-            }} 
-             onFocus={(e)=>{
-                setCreditCardState({...CreditCardState,focus:e.target.name})
-             }}
-            />
-            <input type="tel" name="cvc" placeholder='CVC' value={CreditCardState.cvc} onChange={(e)=>{
-                setCreditCardState({...CreditCardState,cvc:e.target.value})
-            }} 
-             onFocus={(e)=>{
-                setCreditCardState({...CreditCardState,focus:e.target.name})
-             }}
-            />
-      </form>
+        
     </div>
   )
 }
